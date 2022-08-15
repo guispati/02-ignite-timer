4b1c2c12fd2a747b4f5ae3ecf7d20e9662d1fcfe
Guilherme Spati <guilherme.spati723@gmail.com>
2022-08-14 22:58:27 -0300

    aplicacao finalizada

--- src/pages/Home/components/Countdown/index.tsx
+++ src/pages/Home/components/Countdown/index.tsx
@@ -1,6 +1,6 @@
 import { differenceInSeconds } from "date-fns";
 import { useEffect, useContext } from "react";
-import { CyclesContext } from "../..";
+import { CyclesContext } from "../../../../contexts/CyclesContext";
 import { CountdownContainer, Separator } from "./styles";
 
 export function Countdown() {
@@ -13,7 +13,7 @@ export function Countdown() {
 
         if (activeCycle) {
             interval = setInterval(() => {
-                const secondsDifference = differenceInSeconds(new Date(), activeCycle.startDate);
+                const secondsDifference = differenceInSeconds(new Date(), new Date(activeCycle.startDate));
 
                 if (secondsDifference >= totalSeconds) {
                     markCurrentCycleAsFinished();
